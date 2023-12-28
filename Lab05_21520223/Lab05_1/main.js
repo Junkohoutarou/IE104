80998ad049388665020e2a13728a286fd422cecd
Nguyen Phu Hao <126962580+Junkohoutarou@users.noreply.github.com>
2023-12-28 22:45:31 +0700

    Update main.js

--- Lab05_21520223/Lab05_1/main.js
+++ Lab05_21520223/Lab05_1/main.js
@@ -5,7 +5,6 @@ function randomValueFromArray(array) {
   const random = Math.floor(Math.random() * array.length);
   return array[random];
 }
-
 const storyText =
   "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
 const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
